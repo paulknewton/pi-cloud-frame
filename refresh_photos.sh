a41106cc9761f8dc18bdc01df19d65f9edff1713
Paul Newton <paul@topolino.org>
2019-07-18 08:49:43 +0200

    Removed raw/cropped folders from cmd line

--- refresh_photos.sh
+++ refresh_photos.sh
@@ -3,9 +3,9 @@
 # Download a set of random photos from icloud and crop to the correct aspect ratio
 # usage: refresh_photos.sh icloud_id icloud_pwd download_folder crop_folder sample_size
 
-DOWNLOAD=$3			# folder to store raw downloaded photos
-CROPPED=$4			# folder to store re-cropped photos (with correct aspect ratio)
-SAMPLE_SIZE=$5	# number of photos to download
+DOWNLOAD=raw			# folder to store raw downloaded photos
+CROPPED=cropped		# folder to store re-cropped photos (with correct aspect ratio)
+SAMPLE_SIZE=$3		# number of photos to download
 
 # empty the current set of raw images (normally already done)
 rm -f "$DOWNLOAD"/*
