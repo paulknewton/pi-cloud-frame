76c223ab3e4f304f8f484e325965b99d68ee02b4
Paul Newton <paul@topolino.org>
2019-07-21 00:52:15 +0200

    Fixed cmd line

--- refresh_photos.sh
+++ refresh_photos.sh
@@ -6,12 +6,13 @@
 DOWNLOAD=raw			# folder to store raw downloaded photos
 CROPPED=cropped		# folder to store re-cropped photos (with correct aspect ratio)
 SAMPLE_SIZE=$3		# number of photos to download
+ALBUM=photoframe
 
 # empty the current set of raw images (normally already done)
 rm -f "$DOWNLOAD"/*
 
 # download photos from icloud
-python3 icloud_photos.py "$1" "$2" "$DOWNLOAD" $SAMPLE_SIZE
+python3 icloud_photos.py "$1" "$2" --output "$DOWNLOAD" --album $ALBUM --sample $SAMPLE_SIZE
 
 # empty the current set of cropped images (currently in use)
 rm -f "$CROPPED"/*
@@ -19,7 +20,7 @@ rm -f "$CROPPED"/*
 # crop the new photos
 echo "Cropping photos from $DOWNLOAD to $CROPPED..."
 cd "$CROPPED"
-for i in "../$DOWNLOAD"/*; do echo `basename $i`; aspectcrop -a 16:9 "$i" `basename $i`; done
+for i in "../$DOWNLOAD"/*; do echo `basename $i`; /home/pi/pi-cloud-frame/aspectcrop -a 800:480 "$i" `basename $i`; done
 cd ..
 
 # tidy up the raw photos
