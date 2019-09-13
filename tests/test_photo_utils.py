b75d6ddfc007ed48c88d139884473465c196e564
Paul Newton <paul@topolino.org>
2019-09-13 13:53:47 +0200

    Update test_photo_utils.py

--- tests/test_photo_utils.py
+++ tests/test_photo_utils.py
@@ -86,7 +86,7 @@ def test_invalid_exif_rotation():
 
 
 def test_no_exif_orientation():
-    portrait_file = "portrait_no_exif.jpg"
+    portrait_file = "tests/portrait_no_exif.jpg"
 
     assert pu.get_file_exif_orientation(portrait_file) is None
 
