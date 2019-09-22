6c00b2bcb92d1031aeecb3b7600d5f61268e5ac0
Paul Newton <paul@topolino.org>
2019-09-22 22:12:27 +0200

    Added MPU6050 sensor

---
+++ tests/run_mpu6050.py
@@ -0,0 +1,7 @@
+from utils import mpu6050
+
+if __name__ == "__main__":
+
+    accelerometer = mpu6050.Mpu6050()
+    for i in range(10):
+        print(accelerometer.get_rotation(), accelerometer.get_rotation_simple())
