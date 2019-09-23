2f850e5c40d1fa6f8a806e4bc190fb138dce60de
paulknewton <paul@topolino.org>
2019-09-23 23:28:08 +0200

    Additional unit tests

--- tests/test_frame.py
+++
@@ -1,7 +0,0 @@
-from gui import media_players
-
-
-def skip_test_playlist():
-    player = media_players.PhotoPlayer("test_photo_player", "tests/test_photos")
-    assert player.get_folder() == "tests/test_photos"
-    assert player.get_playlist() == ["1.jpg", "2.jpg"]
