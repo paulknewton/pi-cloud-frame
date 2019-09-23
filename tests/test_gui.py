df1eefdd8a67a874b4b8a321bbbc9a36cf631dc2
paulknewton <paul@topolino.org>
2019-09-24 00:55:39 +0200

    GUI unit tests

--- tests/test_gui.py
+++ tests/test_gui.py
@@ -16,12 +16,29 @@ def test_change_players(qtbot):
     assert type(window.get_current_player()) == gui.media_players.PhotoPlayer
     assert window.get_current_player().get_name() == "Photo Player 1"
 
+    # up
     qtbot.keyPress(window, QtCore.Qt.Key_Up)
     assert window.get_current_player().get_name() == "Photo Player 2"
 
+    # down
     qtbot.keyPress(window, QtCore.Qt.Key_Down)
     assert window.get_current_player().get_name() == "Photo Player 1"
 
 
-#def test_show_photos(qtbot):
+def test_cycle_players(qtbot):
+    window = PhotoFrame(Config("tests/test_config.yml"))
+    assert type(window.get_current_player()) == gui.media_players.PhotoPlayer
+    assert window.get_current_player().get_name() == "Photo Player 1"
+
+    # up x2
+    qtbot.keyPress(window, QtCore.Qt.Key_Up)
+    qtbot.keyPress(window, QtCore.Qt.Key_Up)
+    assert window.get_current_player().get_name() == "Photo Player 1"
+
+    # down x2
+    qtbot.keyPress(window, QtCore.Qt.Key_Down)
+    qtbot.keyPress(window, QtCore.Qt.Key_Down)
+    assert window.get_current_player().get_name() == "Photo Player 1"
+
+# def test_show_photos(qtbot):
 #    qtbot.wait(5000)
