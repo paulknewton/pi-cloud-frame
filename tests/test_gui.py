24260253e8f75bd1bdf3d38e7142914f39fb750a
paulknewton <paul@topolino.org>
2019-09-24 00:50:04 +0200

    GUI unit tests

---
+++ tests/test_gui.py
@@ -0,0 +1,27 @@
+from PyQt5 import QtCore
+
+import gui.media_players
+from gui.config import Config
+from gui.photo_app import PhotoFrame
+
+
+# @pytest.fixture
+# def setup():
+#    window = PhotoFrame(Config("tests/test_config.yml"))
+#
+
+
+def test_change_players(qtbot):
+    window = PhotoFrame(Config("tests/test_config.yml"))
+    assert type(window.get_current_player()) == gui.media_players.PhotoPlayer
+    assert window.get_current_player().get_name() == "Photo Player 1"
+
+    qtbot.keyPress(window, QtCore.Qt.Key_Up)
+    assert window.get_current_player().get_name() == "Photo Player 2"
+
+    qtbot.keyPress(window, QtCore.Qt.Key_Down)
+    assert window.get_current_player().get_name() == "Photo Player 1"
+
+
+#def test_show_photos(qtbot):
+#    qtbot.wait(5000)
