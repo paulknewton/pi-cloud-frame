d1999f30ccefeec39ca4f8ac5a6396dfbcd0038a
paulknewton <paul@topolino.org>
2019-10-02 01:01:42 +0200

    Added machine stats

--- gui/dashboard.py
+++ gui/dashboard.py
@@ -1,5 +1,7 @@
-from typing import List
+from hurry.filesize import size
+import psutil
 import logging
+from typing import List
 
 from PyQt5 import QtWidgets, QtCore
 from PyQt5.QtWidgets import QSizePolicy
@@ -20,10 +22,12 @@ class FrameDashboard(PhotoFrameContent):
         # self.main_window.setSizePolicy(QSizePolicy(QSizePolicy.Fixed, QSizePolicy.Fixed))
         # self.main_window.setFixedSize(self.photo_frame.frame_size)
 
-        self.summary_widget = None
+        self.machine_summary_widget = None
+        self.frame_summary_widget = None
         self.player_list_widget = None
 
         self._build_ui()
+        self._update()
 
     def _build_ui(self):
         main_layout = QtWidgets.QVBoxLayout(self.main_window)
@@ -41,18 +45,31 @@ class FrameDashboard(PhotoFrameContent):
         heading.setSizePolicy(no_vstretch_policy)
         main_layout.addWidget(heading)
 
-        summary_frame = QtWidgets.QFrame()
-        summary_frame.setFrameStyle(QtWidgets.QFrame.Panel)
-        summary_frame_layout = QtWidgets.QVBoxLayout(summary_frame)
-        summary_frame.setLayout(summary_frame_layout)
-        summary_frame.setSizePolicy(no_vstretch_policy)
-        summary_frame.setFixedWidth(self.photo_frame.frame_size.width() * 0.7)
+        machine_summary_widget = QtWidgets.QFrame()
+        machine_summary_widget.setFrameStyle(QtWidgets.QFrame.Panel)
+        machine_summary_widget_layout = QtWidgets.QVBoxLayout(machine_summary_widget)
+        machine_summary_widget.setLayout(machine_summary_widget_layout)
+        machine_summary_widget.setSizePolicy(no_vstretch_policy)
+        machine_summary_widget.setFixedWidth(self.photo_frame.frame_size.width() * 0.7)
 
-        self.summary_widget = QtWidgets.QLabel(summary_frame)
-        self.summary_widget.setAlignment(QtCore.Qt.AlignLeft)
-        summary_frame_layout.addWidget(self.summary_widget)
+        self.machine_summary_widget = QtWidgets.QLabel(machine_summary_widget)
+        self.machine_summary_widget.setAlignment(QtCore.Qt.AlignLeft)
+        machine_summary_widget_layout.addWidget(self.machine_summary_widget)
 
-        main_layout.addWidget(summary_frame)
+        main_layout.addWidget(machine_summary_widget)
+
+        frame_summary_widget = QtWidgets.QFrame()
+        frame_summary_widget.setFrameStyle(QtWidgets.QFrame.Panel)
+        frame_summary_widget_layout = QtWidgets.QVBoxLayout(frame_summary_widget)
+        frame_summary_widget.setLayout(frame_summary_widget_layout)
+        frame_summary_widget.setSizePolicy(no_vstretch_policy)
+        frame_summary_widget.setFixedWidth(self.photo_frame.frame_size.width() * 0.7)
+
+        self.frame_summary_widget = QtWidgets.QLabel(frame_summary_widget)
+        self.frame_summary_widget.setAlignment(QtCore.Qt.AlignLeft)
+        frame_summary_widget_layout.addWidget(self.frame_summary_widget)
+
+        main_layout.addWidget(frame_summary_widget)
 
         player_frame = QtWidgets.QFrame()
         player_frame.setFrameStyle(QtWidgets.QFrame.Panel)
@@ -65,13 +82,32 @@ class FrameDashboard(PhotoFrameContent):
 
         main_layout.addWidget(player_frame)
 
-        self._update_summary()
-        self._update_player_list()
-
     def get_main_widget(self):
         return self.main_window
 
-    def _update_summary(self):
+    def _update_machine_summary(self):
+        memory = psutil.virtual_memory()
+        disk = psutil.disk_usage("/")
+
+        summary_entries = [
+            "<b>CPU load:</b> %s%%" % psutil.cpu_percent(),
+            "<b>Total memory:</b> %s" % size(memory.total),
+            "<b>Available memory:</b> %s" % size(memory.available),
+            "<b>Total disk space:</b> %s" % size(disk.total),
+            "<b>Used disk space:</b> %s" % size(disk.used),
+            "<b>Free disk space:</b> %s" % size(disk.free)
+        ]
+
+        if hasattr(psutil, "sensors_temperatures"):
+            temp = psutil.sensors_temperatures()
+            summary_entries.append(["<b>CPU temp</b> %s" % temp])
+
+        summary_text = "<br>".join(summary_entries)
+        logger.info(summary_text)
+
+        self.machine_summary_widget.setText(summary_text)
+
+    def _update_frame_summary(self):
         summary_entries = [
             "<b>Number of players:</b> %d" % len(self.photo_frame.players),
             "<b>Slideshow delay:</b> %d" % self.photo_frame.slideshow_delay,
@@ -84,13 +120,13 @@ class FrameDashboard(PhotoFrameContent):
 
         # self.db_content.setSizePolicy(QSizePolicy(QSizePolicy.Fixed, QSizePolicy.Fixed))
         # self.db_content.setFixedSize(self.photo_frame.width() * 0.7, self.photo_frame.height())
-        self.summary_widget.setText(summary_text)
+        self.frame_summary_widget.setText(summary_text)
 
     def _update_player_list(self):
         player_list_entries = ["<b>Player list:</b>"] + [self._get_player_entry(player) for player in
                                                          self.photo_frame.players]
         player_list_text = "<br>".join(player_list_entries)
-        logger.info(player_list_text)
+        logger.debug(player_list_text)
         self.player_list_widget.setText(player_list_text)
 
     def _get_player_entry(self, player: PhotoFrameContent):
@@ -99,7 +135,8 @@ class FrameDashboard(PhotoFrameContent):
 
     def _update(self):
         logger.debug("Updating dashboard")
-        self._update_summary()
+        self._update_machine_summary()
+        self._update_frame_summary()
         self._update_player_list()
 
     def next(self):
