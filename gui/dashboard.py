d5c55352bb0a0e728958d49a1e78e7415e8a643e
Paul Newton <paul@topolino.org>
2019-10-02 23:35:56 +0200

    Changed layout

--- gui/dashboard.py
+++ gui/dashboard.py
@@ -22,16 +22,16 @@ class FrameDashboard(PhotoFrameContent):
         # self.main_window.setSizePolicy(QSizePolicy(QSizePolicy.Fixed, QSizePolicy.Fixed))
         # self.main_window.setFixedSize(self.photo_frame.frame_size)
 
-        self.machine_summary_widget = None
-        self.frame_summary_widget = None
-        self.player_list_widget = None
+        self.machine_text = None
+        self.frame_text = None
+        self.player_text = None
 
         self._build_ui()
         self._update()
 
     def _build_ui(self):
         # align each section in a centred, vertical column
-        main_layout = QtWidgets.QVBoxLayout(self.main_window)
+        main_layout = QtWidgets.QGridLayout(self.main_window)
         main_layout.setAlignment(QtCore.Qt.AlignHCenter)
         self.main_window.setLayout(main_layout)
 
@@ -40,62 +40,67 @@ class FrameDashboard(PhotoFrameContent):
         # no_vstretch_policy.setHorizontalPolicy(QSizePolicy.Fixed)
 
         # title
-        heading = QtWidgets.QLabel(self.main_window)
+        heading = QtWidgets.QLabel()
 
         heading.setAlignment(QtCore.Qt.AlignCenter)
         heading.setText("<p><b>Dashboard</b>")
-        heading.setFixedWidth(self.photo_frame.frame_size.width() * 0.7)
+        heading.setFixedWidth(self.photo_frame.frame_size.width() * 0.3)
         heading.setSizePolicy(no_vstretch_policy)
-        main_layout.addWidget(heading)
+        #main_layout.addWidget(heading, 0, 0, 0, 1)
 
         # machine info
-        machine_summary_widget = QtWidgets.QFrame()
-        machine_summary_widget.setFrameStyle(QtWidgets.QFrame.Panel)
-        machine_summary_widget_layout = QtWidgets.QVBoxLayout(machine_summary_widget)
-        machine_summary_widget.setLayout(machine_summary_widget_layout)
-        machine_summary_widget.setSizePolicy(no_vstretch_policy)
-        machine_summary_widget.setFixedWidth(self.photo_frame.frame_size.width() * 0.7)
+        machine_group = QtWidgets.QFrame()
+        machine_group.setFrameStyle(QtWidgets.QFrame.Panel)
+        machine_layout = QtWidgets.QVBoxLayout(machine_group)
+        machine_group.setLayout(machine_layout)
+        machine_group.setSizePolicy(no_vstretch_policy)
+        machine_group.setFixedWidth(self.photo_frame.frame_size.width() * 0.3)
 
-        self.machine_summary_widget = QtWidgets.QLabel(machine_summary_widget)
-        font = machine_summary_widget.font()
+        self.machine_text = QtWidgets.QLabel(machine_group)
+        font = machine_group.font()
         font.setPointSize(font.pointSize() - 2)
-        machine_summary_widget.setFont(font)
-        self.machine_summary_widget.setAlignment(QtCore.Qt.AlignLeft)
-        machine_summary_widget_layout.addWidget(self.machine_summary_widget)
+        machine_group.setFont(font)
+        self.machine_text.setAlignment(QtCore.Qt.AlignLeft)
+        machine_layout.addWidget(self.machine_text)
 
-        main_layout.addWidget(machine_summary_widget)
+        main_layout.addWidget(machine_group, 0, 0)
 
         # frame info
-        frame_summary_widget = QtWidgets.QFrame()
-        frame_summary_widget.setFrameStyle(QtWidgets.QFrame.Panel)
-        frame_summary_widget_layout = QtWidgets.QVBoxLayout(frame_summary_widget)
-        frame_summary_widget.setLayout(frame_summary_widget_layout)
-        frame_summary_widget.setSizePolicy(no_vstretch_policy)
-        frame_summary_widget.setFixedWidth(self.photo_frame.frame_size.width() * 0.7)
+        frame_group = QtWidgets.QFrame()
+        frame_group.setFrameStyle(QtWidgets.QFrame.Panel)
+        frame_layout = QtWidgets.QVBoxLayout(frame_group)
+        frame_group.setLayout(frame_layout)
+        frame_group.setSizePolicy(no_vstretch_policy)
+        frame_group.setFixedWidth(machine_group.size().width())
 
-        self.frame_summary_widget = QtWidgets.QLabel(frame_summary_widget)
-        font = frame_summary_widget.font()
+        self.frame_text = QtWidgets.QLabel(frame_group)
+        font = frame_group.font()
         font.setPointSize(font.pointSize() - 2)
-        frame_summary_widget.setFont(font)
-        self.frame_summary_widget.setAlignment(QtCore.Qt.AlignLeft)
-        frame_summary_widget_layout.addWidget(self.frame_summary_widget)
+        frame_group.setFont(font)
+        self.frame_text.setAlignment(QtCore.Qt.AlignLeft)
+        frame_layout.addWidget(self.frame_text)
 
-        main_layout.addWidget(frame_summary_widget)
+        main_layout.addWidget(frame_group, 0, 1)
 
         # list of players
-        player_frame = QtWidgets.QFrame()
-        player_frame.setFrameStyle(QtWidgets.QFrame.Panel)
-        player_frame_layout = QtWidgets.QVBoxLayout(player_frame)
-        player_frame.setLayout(player_frame_layout)
+        player_group = QtWidgets.QFrame()
+        player_group.setFrameStyle(QtWidgets.QFrame.Panel)
+        player_layout = QtWidgets.QVBoxLayout(player_group)
+        player_group.setLayout(player_layout)
 
-        self.player_list_widget = QtWidgets.QLabel(player_frame)
-        font = self.player_list_widget.font()
+        self.player_text = QtWidgets.QLabel(player_group)
+        self.player_text.setText("hello")
+        font = self.player_text.font()
         font.setPointSize(font.pointSize() - 2)
-        self.player_list_widget.setFont(font)
-        self.player_list_widget.setAlignment(QtCore.Qt.AlignLeft)
-        player_frame_layout.addWidget(self.player_list_widget)
+        self.player_text.setFont(font)
+        self.player_text.setAlignment(QtCore.Qt.AlignLeft)
+        player_layout.addWidget(self.player_text)
 
-        main_layout.addWidget(player_frame)
+        # add a scrollbar
+        #scroll = QtWidgets.QScrollArea(player_group)
+        #scroll.setWidget(self.player_text)
+
+        main_layout.addWidget(player_group, 1, 0, 1, 2)
 
     def get_main_widget(self):
         return self.main_window
@@ -120,7 +125,7 @@ class FrameDashboard(PhotoFrameContent):
         summary_text = "<br>".join(summary_entries)
         logger.debug(summary_text)
 
-        self.machine_summary_widget.setText(summary_text)
+        self.machine_text.setText(summary_text)
 
     def _update_frame_summary(self):
         summary_entries = [
@@ -137,13 +142,13 @@ class FrameDashboard(PhotoFrameContent):
 
         # self.db_content.setSizePolicy(QSizePolicy(QSizePolicy.Fixed, QSizePolicy.Fixed))
         # self.db_content.setFixedSize(self.photo_frame.width() * 0.7, self.photo_frame.height())
-        self.frame_summary_widget.setText(summary_text)
+        self.frame_text.setText(summary_text)
 
     def _update_player_list(self):
         player_list_entries = [self._get_player_entry(player) for player in self.photo_frame.players]
         player_list_text = "<br>".join(player_list_entries)
         logger.debug(player_list_text)
-        self.player_list_widget.setText(player_list_text)
+        self.player_text.setText(player_list_text)
 
     def _get_player_entry(self, player: PhotoFrameContent):
         properties = player.get_properties()
