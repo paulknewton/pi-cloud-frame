b56468c393d8347d752b949d033fa73c53f91b1d
Paul Newton <paul@topolino.org>
2021-08-27 23:37:35 +0200

    Fixed GPS lookup

--- tests/test_gps_lookup.py
+++ tests/test_gps_lookup.py
@@ -3,5 +3,5 @@ from utils import photo_utils
 
 def test_gps_location():
     gps_data = [50, 49, 859 / 100, "N", 0, 8, 249 / 20, "W"]
-    expected = "Big Fish Trading Co., Grand Junction Road, Queen's Park, Brighton, Brighton and Hove, South East, England, BN2 1TD, United Kingdom"
+    expected = "Big Fish Trading Co., Grand Junction Road, Queen's Park, Brighton, Brighton and Hove, South East England, England, BN2 1TD, United Kingdom"
     assert photo_utils.get_gps_dms_location(*gps_data) == expected
